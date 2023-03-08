d440fa73fd85a94d77db79127cb6b2d3077863a3
Jasmine Nketiah <jasmine.nketiah@gmail.com>
2023-03-07 20:34:09 -0600

    working on Q9

--- scripts/Lahman.sql
+++ scripts/Lahman.sql
@@ -150,14 +150,72 @@ GROUP BY
 	b.cs
 HAVING CAST(sb AS numeric) + CAST(cs AS numeric) >= 20
 ORDER BY success DESC;
-------
+
+-- Answer : Chris Owings, 91%
 
 
+--7.From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time? #COME BACK TO Q7 TO COMPLETE IT
 
+SELECT
+	DISTINCT name,
+	yearid,
+	wswin,
+	w
+FROM teams
+WHERE yearid >= 1970
+	AND wswin NOT LIKE 'Y'
+ORDER BY w DESC;
 
---7.From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
+--Answer : largest number of wins for a team is 116 by Seattle Mariners
+
 
 --8.Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
+
+SELECT
+	p.park_name,
+	h.team,
+	ROUND((CAST(h.attendance AS numeric) / CAST(h.games AS numeric))) AS avg_att,
+	h.games
+FROM homegames AS h
+LEFT JOIN parks AS p
+ON h.park = p.park
+WHERE h.year = 2016
+GROUP BY
+	p.park_name,
+	h.team,
+	h.games,
+	h.attendance
+HAVING games >= 10
+ORDER BY avg_att DESC
+LIMIT 5;
+
+--Answer: Top 5 
+--Dodger Stadium (Los Angeles Dodgers) at 45,720
+--Busch Stadium III (St Louis Cardinals) at 42,525
+--Rogers Center (Toronto Blue Jays) at 41,878
+--AT&T Park (San Francisco Giants) at 41,546
+--Wrigley Field (Chicago Cubs) at 39,906
+
+
+SELECT
+	p.park_name,
+	h.team,
+	ROUND((CAST(h.attendance AS numeric) / CAST(h.games AS numeric))) AS avg_att,
+	h.games
+FROM homegames AS h
+JOIN parks AS p
+ON h.park = p.park
+WHERE h.year = 2016
+GROUP BY
+	p.park_name,
+	h.team,
+	h.games,
+	h.attendance
+HAVING games >= 10
+ORDER BY avg_att ASC
+LIMIT 5;
+
+--Answer: Lowest 5
 
 --9.Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
 
